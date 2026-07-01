cb7248e62c76656d67e6a5a945f1e8833a4bb488
Shalom Achikasim <shalomachims@gmail.com>
2026-07-01 16:43:47 +0100

    feat(app): implement get blogs query and handler

---
+++ Demo.Application/Blogs/Queries/GetBlogs/BlogVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Blogs.Queries.GetBlogs
+{
+    internal class BlogVm
+    {
+    }
+}
