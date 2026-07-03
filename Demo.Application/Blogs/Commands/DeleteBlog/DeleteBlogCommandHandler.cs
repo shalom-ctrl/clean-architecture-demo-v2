6bffb41753f5526b5202d4f1107d8df626063e60
Shalom Achikasim <shalomachims@gmail.com>
2026-07-03 12:18:09 +0100

    feat(app): add delete blog command and handler

---
+++ Demo.Application/Blogs/Commands/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Demo.Domain.Interface;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Blogs.Commands.DeleteBlog
+{
+    public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Unit>
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public DeleteBlogCommandHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+        public async Task<Unit> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+        {
+            await _blogRepository.DeleteBlogAsync(request.Id);
+            return Unit.Value;
+        }
+    }
+}
